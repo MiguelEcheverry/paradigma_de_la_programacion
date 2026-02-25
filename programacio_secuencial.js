2d514bd3427c5b9b651a68f0a88a79583906ff87
Miguel Echeverry <miguel2eche.tic@gmail.com>
2026-02-25 16:11:47 -0500

    bsdsnifwifrwi

---
+++ programacio_secuencial.js
@@ -0,0 +1,18 @@
+let nombre = "tolerancia";
+let age = 40;
+let last_name = "emerita";
+let num = 678395678638;
+function sumar (a,b){
+    return a + b;
+}
+function restar (a,b){
+let operacion = a- b;
+    return operacion;
+}
+
+console.log (sumar(6, 8))
+let resultadoresta = restar(5,4);
+
+console.log ("el resultado de la resta es:" + resultadoresta )
+
+console.log("nombre completo:" + nombre + " " + last_name + " " + "edad:" + age )
